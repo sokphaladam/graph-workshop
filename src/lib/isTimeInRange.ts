fd3e19909d4e9865f12d742a5107f7975276aaae
sokphaladam <sokphaladam@gmail.com>
2024-11-04 15:24:07 +0700

    hot fix

--- src/lib/isTimeInRange.ts
+++ src/lib/isTimeInRange.ts
@@ -1,8 +1,8 @@
 export function isTimeInRange(startTime, endTime, checkTime) {
   // Convert times to Date objects
-  const start = new Date(`1970-01-01T${startTime}:00`);
-  const end = new Date(`1970-01-01T${endTime}:00`);
-  const check = new Date(`1970-01-01T${checkTime}:00`);
+  const start = new Date(`1970-01-01T${startTime}`);
+  const end = new Date(`1970-01-01T${endTime}`);
+  const check = new Date(`1970-01-01T${checkTime}`);
 
   // Check if the checkTime is between startTime and endTime
   return check >= start && check <= end;
