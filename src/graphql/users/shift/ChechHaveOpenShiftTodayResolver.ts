b29351d900f8001785a46a64186e40b706a8cc68
sokphaladam <sokphaladam@gmail.com>
2026-02-28 16:32:10 +0700

    update

--- src/graphql/users/shift/ChechHaveOpenShiftTodayResolver.ts
+++ src/graphql/users/shift/ChechHaveOpenShiftTodayResolver.ts
@@ -1,13 +1,15 @@
 import { ContextType } from "src/ContextType";
 import { Formatter } from "src/lib/Formatter";
 
-export async function CheckHaveOpenShiftTodayResolver(_, {}, ctx: ContextType) {
+export async function CheckHaveOpenShiftTodayResolver(
+  _: any,
+  {},
+  ctx: ContextType,
+) {
   const knex = ctx.knex.default;
-  const user = ctx.auth?.id;
 
   const item = await knex
     .table("shift")
-    .where("user_id", user)
     .whereRaw(
       `
     DATE(shift.open) = :open
